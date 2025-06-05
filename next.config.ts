f7d2da566219358fa96a703a94a67df331b2fbc3
dOtOb9 <dotob9@gmail.com>
2025-06-06 03:49:14 +0900

    install mdx

--- next.config.ts
+++ next.config.ts
@@ -15,3 +15,9 @@ const nextConfig: NextConfig = {
 };
 
 module.exports = nextConfig;
+
+const withMDX = require('@next/mdx')();
+module.exports = withMDX({
+  // 他のNext.js設定
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+});
