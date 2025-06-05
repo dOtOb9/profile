a2c73631abe41d4686c6caa2e152da033f689c20
dOtOb9 <dotob9@gmail.com>
2025-06-06 04:48:41 +0900

    add prifile mdx file

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,9 @@ import type { MDXComponents } from 'mdx/types'
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: (props) => <h1 className="text-3xl font-bold my-4 " {...props} />,
+    h2: (props) => <h2 className="text-2xl font-semibold my-3" {...props} />,
+    h3: (props) => <h3 className="text-xl font-medium my-2" {...props} />,
     ...components,
   }
 }
