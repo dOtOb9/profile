3063f62520c9d16fb77234e9c9849f592f5df2cf
dOtOb9 <dotob9@gmail.com>
2025-06-08 18:49:00 +0900

    add search in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import ProfileCard from "@/components/ProfileCard";
-import Image from "next/image";
+import SearchField from "@/components/SearchField";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 p-4">
       <ProfileCard />
+      <SearchField />
     </div>
   );
 }
