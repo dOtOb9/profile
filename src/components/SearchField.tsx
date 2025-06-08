69a85879fb067c1baed7b3459ef3f1ca0071dd93
dOtOb9 <dotob9@gmail.com>
2025-06-09 06:53:21 +0900

    add SearchPageList

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -3,13 +3,15 @@
 import { useState } from 'react';
 
 import SearchBox from './SearchBox';
+import SearchPageList from './SearchPageList';
 
-export default function SearchField() {
+export default function SearchField(  category: string = '') {
   const [query, setQuery] = useState('');
 
   return (
     <div className="flex items-center justify-center w-full p-4">
       <SearchBox setQuery={setQuery} />
+      <SearchPageList query={query} category={category} />
     </div>
   );
 }
---
+++ src/components/SearchPageList.tsx
@@ -0,0 +1,12 @@
+"use client";
+import SearchBox from './SearchBox';
+
+export default function SearchPageList({ query, category }: { query: string; category: string }) {
+
+  return (
+    <div>
+      <ul className="w-full mt-4">
+      </ul>
+    </div>
+  );
+}
