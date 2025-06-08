1a7084e7d34d19bf534560aa2809dd279a17c389
dOtOb9 <dotob9@gmail.com>
2025-06-09 04:56:49 +0900

    add useState in Search Box

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import { useState } from 'react';
+
 export default function SearchBox() {
+  const [query, setQuery] = useState('');
+
   return (
     <div className="flex items-center justify-center w-full p-4">
       <input
         type="text"
         placeholder="Search..."
         className="w-full p-3 border border-gray-300 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
     </div>
   );
