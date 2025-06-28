3d1753e4e783f9cb21bdeddf07905de0cad4244e
dOtOb9 <dotob9@gmail.com>
2025-06-29 04:23:49 +0900

    fix search box

--- src/app/posts/about/page.tsx
+++ src/app/posts/about/page.tsx
@@ -15,7 +15,7 @@ export default function About() {
                       <Me />
                   </MarkdownPackage>
                 </div>
-                <SearchField category="about" />
+                <SearchField />
               </div>
             </div>
         </div>
