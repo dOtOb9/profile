911a8046b2264a1ffcbfa20d0a115429550ca177
dOtOb9 <dotob9@gmail.com>
2025-09-19 19:37:39 +0900

    modify home text -> black

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,9 +4,9 @@ export default function AboutMe({ onClick }: { onClick?: () => void }) {
   return (
     <div className="flex flex-row items-center justify-center hover:text-blue-500 hover:cursor-pointer" onClick={onClick}>
       <div className="py-16 animate-bounce font-bold text-5xl">
-        <button className="rotate-90">＞</button>
+        <button className="rotate-90 text-black">＞</button>
       </div>
-      <p className="text-3xl mb-4">About Me</p>
+      <p className="text-3xl mb-4 text-black">About Me</p>
     </div>
   );
 }
