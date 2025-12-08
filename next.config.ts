73070b763937644b2c9e5317fa00ec6543527c1b
dOtOb9 <dotob9@gmail.com>
2025-12-08 18:10:55 +0900

    バグ修正

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,32 @@
 import type { NextConfig } from "next";
-import remarkGfm from 'remark-gfm';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import rehypePrism from "rehype-prism-plus";
 
-const withMDX = require('@next/mdx')(
-  {
+const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
     options: {
-      remarkPlugins: [remarkMath, remarkGfm],
-      rehypePlugins: [rehypeKatex, rehypePrism],
+        remarkPlugins: [
+            require.resolve('remark-math'),
+            require.resolve('remark-gfm'),
+        ],
+        rehypePlugins: [
+            require.resolve('rehype-katex'),
+            require.resolve('rehype-prism-plus'),
+        ],
     },
-  }
-);
+});
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-        pathname: '/**',
-      },
-    ],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+                pathname: '/**',
+            },
+        ],
+    },
+    turbopack: {},
+    
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
 
 module.exports = withMDX(nextConfig);
